3d9b68ac1251f34a58cca6aba39ee3aea8f09ae8
ZANE <130233619+chochozane@users.noreply.github.com>
2023-11-14 17:55:49 +0900

    231114_1755

--- DunDunDungeon/Item.cs
+++ DunDunDungeon/Item.cs
@@ -6,14 +6,94 @@
         class Item
         {
             public string Name { get; }
-            public string Effect { get; }
             public string Description { get; }
+            public int Type { get; }
+            public int Atk { get; }
+            public int Def { get; }
+            public int Hp { get; }
+            public bool IsEquipped { get; set; }
 
-            public Item(string newName, string newEffect, string newDescription)
+            public static int ItemCnt = 0;
+
+            public Item(string name, string description, int type, int atk, int def, int hp, bool isEquipped = false)
             {
-                Name = newName;
-                Effect = newEffect;
-                Description = newDescription;
+                Name = name;
+                Description = description;
+                Type = type;
+                Atk = atk;
+                Def = def;
+                Hp = hp;
+                IsEquipped = isEquipped;
+            }
+
+            public void PrintItemStatDescription(bool withNumber = false, int idx = 0) // withNumber 는 장착 관리와 관련한 파라미터입니다 !
+            {
+                Console.Write("- ");
+                if (withNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write("{0}. ", idx);
+                    Console.ResetColor();
+                }
+                if (IsEquipped)
+                {
+                    Console.Write("[");
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("E");
+                    Console.ResetColor();
+                    Console.Write("]");
+                    Console.Write(PadRightForMixedText(Name, 15)); // 장착 중인 경우 9글자 출력.
+                }
+                else
+                {
+                    Console.Write(PadRightForMixedText(Name, 18)); // 여긴 이미 Item 안에 있기 때문에 Item.Name 이 아니고 Name ! & 장착 중이 아닌 경우 12글자 출력.
+                }
+                    Console.Write(" | ");
+
+
+                // 삼항연산자 - 조건 ? 조건이 참이라면 : 조건이 거짓이라면
+                if (Atk != 0)
+                {
+                    Console.Write($"Atk {(Atk >= 0 ? "+" : "")}{Atk}");
+                }
+                if (Def != 0)
+                {
+                    Console.Write($"Def {(Def >= 0 ? "+" : "")}{Def}");
+                }
+                if (Hp != 0)
+                {
+                    Console.Write($"Hp {(Hp >= 0 ? "+" : "")}{Hp}");
+                }
+
+                Console.Write(" | ");
+                Console.WriteLine(Description);
+            }
+
+            // Padding 관련 함수
+            public static int GetPrintableLength(string str)
+            {
+                int length = 0;
+                foreach (char c in str)
+                {
+                    if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+                    {
+                        length += 2; // 한글과 같은 넓은 문자에 대해 길이를 2 로 취급.
+                    }
+                    else
+                    {
+                        length += 1; // 나머지 문자에 대해 길이를 1 로 취급.
+                    }
+                }
+                return length;
+            }
+
+
+            // Padding 관련 함수
+            public static string PadRightForMixedText(string str, int totalLength) 
+            {
+                int currentLength = GetPrintableLength(str);
+                int padding = totalLength - currentLength;
+                return str.PadRight(str.Length + padding);
             }
         }
     }
--- DunDunDungeon/Program.cs
+++ DunDunDungeon/Program.cs
@@ -1,15 +1,11 @@
-﻿
-using ConsoleTables;
-
-namespace DunDunDungeon
+﻿namespace DunDunDungeon
 {
 
     internal partial class Program
     {
 
-        private static Character player1;
-        private static Item muIronArmor;
-        private static Item oldSword;
+        static Character player1;
+        static Item[] _items;
 
 
         static void Main(string[] args)
@@ -25,18 +21,33 @@ namespace DunDunDungeon
             player1 = new Character("너의이름은", 01, "전사", 10, 5, 100, 1500);
 
             // 아이템 설명
-            muIronArmor = new Item("무쇠갑옷", "방어력 +5", "무쇠로 만들어져 튼튼한 갑옷입니다.");
-            oldSword = new Item("낡은 검", "공격력 +2", "쉽게 볼 수 있는 낡은 검입니다.");
+            _items = new Item[10];
+            AddItem(new Item("무쇠갑옷", "무쇠로 만들어져 튼튼한 갑옷입니다.", 0, 0, 5, 0));
+            AddItem(new Item("낡은 검", "쉽게 볼 수 있는 낡은 검입니다.", 1, 2, 0, 0));
+            AddItem(new Item("엄마의 후라이팬", "꽤나 강력하고 뜨거운 후라이팬입니다.", 1, 5, 0, 0));
+        }
+
+        // Item 배열에 아이템을 추가하기 위해 만든 함수
+        static void AddItem(Item item)
+        {
+            if (Item.ItemCnt == 10)
+            {
+                return; // 아이템이 꽉 차는 경우, 아무일도 일어나지 않는다 !
+            }
+            _items[Item.ItemCnt] = item;
+            Item.ItemCnt++;
         }
 
 
         // 게임 시작 화면
         static void ShowGameStartScene()
         {
-            Console.WriteLine("DunDunDungeon");
+            Console.WriteLine("■■■■■■■■■■");
+            Console.WriteLine("Sparta DunDunDungeon");
+            Console.WriteLine("■■■■■■■■■■");
             Console.WriteLine("\n");
-            Console.WriteLine("Press Enter to Start !");
-            Console.ReadLine();
+            Console.WriteLine("Press Any Key to Start !");
+            Console.ReadKey();
         }
 
 
@@ -46,69 +57,133 @@ namespace DunDunDungeon
             Console.Clear();
 
             Console.WriteLine("스파르타 마을에 오신 여러분 환영합니다.");
-            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.\n");
-            Console.WriteLine("1. 상태 보기\n2. 인벤토리\n");
-            Console.WriteLine("원하시는 행동을 입력해주세요.(숫자만 입력해주세요!)");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write(">> ");
-            Console.ResetColor();
-            int input = int.Parse(Console.ReadLine());
+            Console.WriteLine("이곳에서 던전으로 들어가기 전 활동을 할 수 있습니다.");
+            Console.WriteLine();
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine("2. 인벤토리");
+            Console.WriteLine();
 
-            switch (input)
+            switch (CheckValidInput(1, 2))
             {
                 case 1:
                     ShowStatus(); // 상태 보기
                     break;
-
                 case 2:
                     ShowInvent(); // 인벤토리
-                    break;
-
-                default:
-                    Console.WriteLine("잘못된 입력입니다.");
-                    // 아직 메뉴 이동 연결 못 시킴 !
-                    Console.WriteLine("1. 상태보기\n2. 인벤토리\n");
-                    Console.WriteLine("다시 입력해주세요.(숫자만 입력해주세요!)");
                     break;
             }
         }
 
+        static int CheckValidInput(int min, int max)
+        {
+            int keyInput; // TryParse 에서 사용
+            bool result; // while 문에서 사용
+
+            do
+            {
+                Console.WriteLine("원하시는 행동을 입력해주세요.(숫자만 입력해주세요!)");
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write(">> ");
+                Console.ResetColor();
+                result = int.TryParse(Console.ReadLine(), out keyInput); // TryParse 는 될 수도 있고 안 될 수도 있고를 생각하는 것. 여기서는 숫자일 수도 아닐 수도 있다는걸 생각한다는 의미. ReadLine 을 한 다음에 그게 맞다면 가져오고(keyInput 으로 !), 아니면 안 가져오고 !)
+            }
+            while (result == false || CheckIfValid(keyInput, min, max) == false); // false 면 계속 반복을 돈다 ~
+
+            // 여기에 도착했다는 것은 값을 제대로 잘 입력했다는 것 ! :-)
+            return keyInput;
+        }
+
+        private static bool CheckIfValid(int checkable, int min, int max)
+        {
+            if (min <= checkable && checkable <= max)
+            {
+                return true;
+            }
+            return false;
+        }
+
 
         // 상태 보기
-        static void ShowStatus()
+        private static void ShowStatus()
         {
             Console.Clear();
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("상태 보기"); // text color and weight 변경해보자!
-            Console.ResetColor();
-
+            ShowHighlightedText("상태 보기");
             Console.WriteLine("캐릭터의 정보가 표시됩니다");
             Console.WriteLine();
             Console.WriteLine($"{player1.name} ({player1.job})");
-            Console.WriteLine($"Lv. {player1.level}");
-            Console.WriteLine($"공격력 : {player1.atk}");
-            Console.WriteLine($"방어력 : {player1.def}");
-            Console.WriteLine($"체력 : {player1.hp}");
-            Console.WriteLine($"Gold : {player1.gold} G");
+
+            PrintTextWithHighlights("Lv. ", player1.level.ToString("00"));
+            int bonusAtk = getSumBonusAtk();
+            PrintTextWithHighlights("공격력 : ", (player1.atk + bonusAtk).ToString(), bonusAtk > 0 ? string.Format(" (+{0})", bonusAtk) :"");
+            int bonusDef = getSumBonusDef();
+            PrintTextWithHighlights("방어력 : ", (player1.def + bonusDef).ToString(), bonusDef > 0 ? string.Format(" (+{0})", bonusDef) : "");
+            int bonusHp = getSumBonusHp();
+            PrintTextWithHighlights("체력 : ", (player1.hp + bonusHp).ToString(), bonusHp > 0 ? string.Format(" (+{0})", bonusHp) : "");
+            PrintTextWithHighlights("골드 : ", player1.gold.ToString(), " G");
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
-            Console.WriteLine("원하시는 행동을 입력해주세요.(숫자만 입력해주세요!)");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write(">> ");
-            int input = int.Parse(Console.ReadLine());
-            Console.ResetColor();
+            switch(CheckValidInput(0, 0))
+            {
+                case 0:
+                    ShowSpartaVill();
+                    break;
+            }
+        }
 
-            if (input == 0)
+        private static int getSumBonusAtk() // 장착되어있다면 능력치 합쳐주는 역할 !
+        {
+            int sum = 0;
+            for (int i = 0; i < Item.ItemCnt; i++)
             {
-                ShowSpartaVill();
+                if (_items[i].IsEquipped)
+                {
+                    sum += _items[i].Atk;
+                }
             }
-            else
+            return sum;
+        }
+        private static int getSumBonusDef()
+        {
+            int sum = 0;
+            for (int i = 0; i < Item.ItemCnt; i++)
             {
-                Console.WriteLine("잘못된 입력입니다.");
-                // 아직 메뉴 이동 연결 못 시킴 !
+                if (_items[i].IsEquipped)
+                {
+                    sum += _items[i].Def;
+                }
             }
+            return sum;
+        }
+        private static int getSumBonusHp()
+        {
+            int sum = 0;
+            for (int i = 0; i < Item.ItemCnt; i++)
+            {
+                if (_items[i].IsEquipped)
+                {
+                    sum += _items[i].Hp;
+                }
+            }
+            return sum;
+        }
+
+
+        private static void ShowHighlightedText(string text) // 꾸미기 담당 함수
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+        private static void PrintTextWithHighlights(string s1, string s2, string s3 = "") // string s3 는 값이 비어도 되게끔 빈칸을 하나 넣었다 생각하면 됩니다 ~ :-)
+        {
+            Console.Write(s1);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(s2);
+            Console.ResetColor();
+            Console.WriteLine(s3);
         }
 
 
@@ -117,52 +192,29 @@ namespace DunDunDungeon
         {
             Console.Clear();
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("인벤토리"); // text color and weight 변경해보자!
-            Console.ResetColor();
-
+            ShowHighlightedText("인벤토리");
             Console.WriteLine("보유 중인 아이템입니다");
             Console.WriteLine();
             Console.WriteLine("[아이템 목록]");
-            ShowItem();
-            Console.WriteLine();
-            Console.WriteLine("1. 장착 관리");
-            Console.WriteLine("0. 나가기");
-            Console.WriteLine();
-            Console.WriteLine("원하시는 행동을 입력해주세요.(숫자만 입력해주세요!)");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write(">> ");
-            int input = int.Parse(Console.ReadLine());
-            Console.ResetColor();
-
-            switch (input)
+            for (int i =0; i < Item.ItemCnt; i++)
             {
-                case 1:
-                    // 장착 관리로 이동
-                    ShowEquipSetting();
-                    break;
-                case 0:
-                    // 나가기
-                    ShowSpartaVill();
-                    break;
-                default:
-                    Console.WriteLine("잘못된 입력입니다.");
-                    // 아직 메뉴 이동 연결 못 시킴 !
-                    break;
+                _items[i].PrintItemStatDescription();
             }
 
-        }
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine("1. 장착 관리");
+            Console.WriteLine();
 
-
-        // 아이템 목록
-        static void ShowItem()
-        {
-            var table = new ConsoleTable("아이템 이름", "효과", "설명");
-            table.AddRow(muIronArmor.Name, muIronArmor.Effect, muIronArmor.Description);
-            table.AddRow(oldSword.Name, oldSword.Effect, oldSword.Description);
-            // table 의 format 을 default 로 해놓으면 표 하단에 Count 가 나오는데 어떻게 없애지 ??
-            // 일단 table 의 format 을 alternative 으로 바꾸니 표는 덜 예쁘지만 count 는 안 뜬다 !
-            table.Write(Format.Alternative);
+            switch (CheckValidInput(0, 1))
+            {
+                case 0:
+                    ShowSpartaVill();
+                    break;
+                case 1:
+                    ShowEquipSetting();
+                    break;
+            }
         }
 
 
@@ -171,41 +223,37 @@ namespace DunDunDungeon
         {
             Console.Clear();
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-            Console.WriteLine("인벤토리 - 장착 관리"); // text color and weight 변경해보자!
-            Console.ResetColor();
+            ShowHighlightedText("인벤토리 - 장착 관리");
             Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다");
             Console.WriteLine();
             Console.WriteLine("[아이템 목록]");
-            Console.WriteLine("1 [E]무쇠갑옷\t | 방어력 +5 | 무쇠로 만들어져 튼튼한 갑옷입니다.");
-            Console.WriteLine("2 낡은 검\t | 공격력 +2 | 쉽게 볼 수 있는 낡은 검 입니다.");
+            for (int i = 0; i < Item.ItemCnt; i++)
+            {
+                _items[i].PrintItemStatDescription(true, i + 1);
+            }
             Console.WriteLine();
             Console.WriteLine("0. 나가기");
             Console.WriteLine();
-            Console.WriteLine("원하시는 행동을 입력해주세요.(숫자만 입력해주세요!)");
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
-            Console.Write(">> ");
-            int input = int.Parse(Console.ReadLine());
-            Console.ResetColor();
 
-            switch (input)
+            int keyInput = CheckValidInput(0, Item.ItemCnt);
+
+            switch (keyInput)
             {
-                case 1:
-                    Console.WriteLine("기능 구현중입니다 ^_^");
-                    break;
-                case 2:
-                    Console.WriteLine("기능 구현중입니다 ^_^");
-
-                    break;
                 case 0:
                     ShowInvent();
                     break;
                 default:
-                    Console.WriteLine("잘못된 입력입니다.");
-                    // 아직 메뉴 이동 연결 못 시킴 !
+                    ToggleEquipStatus(keyInput - 1); // 유저가 입력하는 값은 1, 2, 3, .. 실제 배열 인덱스는 0, 1, 2, ...
+                    ShowEquipSetting();
                     break;
             }
         }
+
+        private static void ToggleEquipStatus(int idx)
+        {
+            _items[idx].IsEquipped = !_items[idx].IsEquipped;
+        }
+
+       
     }
 }
